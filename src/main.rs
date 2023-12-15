1fc6b0d2c4129ef722f971b791c8d12a3925ef88
Diversable <dan.mantei@outlook.com>
2023-12-15 14:38:43 -0800

    templated crate name in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ async fn axum() -> shuttle_axum::ShuttleAxum {
     use axum::{routing::post, Router};
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use shuttle_leptos::app::*;
-    use shuttle_leptos::fileserv::file_and_error_handler;
+    use {{crate_name}}::app::*;
+    use {{crate_name}}::fileserv::file_and_error_handler;
 
     simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
 
